ef5125897250232945e39b3a3a803983f2ee18c3
Alexander Matrunich <alexander@matrunich.com>
2017-12-09 12:29:54 +0400

    create achievementInput

---
+++ R/achievementInput.R
@@ -0,0 +1,16 @@
+#' Input with achievement description
+#' 
+#' @export
+
+achievementInput <- function(id, desc = "", topics = "", values = "") {
+  
+  ns <- NS(id)
+  
+  tagList(
+    textAreaInput(ns("desc"), "Achievement", value = desc),
+    textInput(ns("topics"), "Topic tags", value = topics),
+    textInput(ns("values"), "Topic values", value = values)
+  )
+  
+  
+}
